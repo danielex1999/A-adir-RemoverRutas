0f488c8330243938f9ea983d902dc73ab8f09ad1
Juan Daniel Cavero Tovar <daniel_jul_can@hotmail.com>
2024-01-29 23:03:10 -0500

    first commit

---
+++ src/main/java/org/bimbo/Main.java
@@ -0,0 +1,40 @@
+package org.bimbo;
+
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //Declaración de variables
+        String rutaChromeDriver = "C:\\Users\\danie\\Documents\\chromedriver.exe";
+        String rutaExcel = "C:\\Users\\danie\\OneDrive\\Escritorio\\VALIDACION 2212.xlsx";
+        FileInputStream fis = new FileInputStream(rutaExcel);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        System.setProperty("webdriver.chrome.driver", rutaChromeDriver);
+        WebDriver driver = new ChromeDriver();
+
+
+
+
+        workbook.close();
+        driver.quit();
+
+    }
+
+
+    private static void saveWorkbook(XSSFWorkbook workbook, String filePath) throws IOException {
+        try (FileOutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        }
+    }
+}
---
+++ src/main/java/org/main/AddRemoveRoutes.java
@@ -0,0 +1,7 @@
+package org.main;
+
+public class AddRemoveRoutes {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
